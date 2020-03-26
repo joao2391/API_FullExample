aee1046719a0428de9583b4dc44f0d037b9abc71
joaopaulo2391 <j_paulo_15@hotmail.com>
2020-03-26 04:36:14 -0300

    Initial Commit

---
+++ src/DevIO.Api/Startup.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using DevIO.Api.Configuration;
+using DevIO.Api.Extensions;
+using DevIO.Data.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MeuDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddIdentityConfiguration(Configuration);
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.WebApiConfig();
+
+            services.AddSwaggerConfig();
+
+            services.AddLoggingConfiguration(Configuration);
+
+            services.ResolveDependencies();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseCors("Development");
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseCors("Development"); // Usar apenas nas demos => Configuração Ideal: Production
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseMvcConfiguration();
+
+            app.UseSwaggerConfig(provider);
+
+            app.UseLoggingConfiguration();
+        }
+    }
+}
